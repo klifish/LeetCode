04bb0a6fa291ef750522a13c6662c3f39aef89c3
kli-cauc <603469018@qq.com>
2016-08-12 08:35:03 +0800

    Update 110BalancedBinaryTree.h
    这道题很有意思！
    主体部分的递归过程，“return isBalanced(root->left) && isBalanced(root->right);”，
    说明在找到一个满足条件的节点（即，非平衡）时，向上返回结果（false）。
    注意，这里需要将结果向上传递至最初的函数调用，需要获取是否存在非平衡节点，因此，只关注false即可

--- Tree/110BalancedBinaryTree.h
+++ Tree/110BalancedBinaryTree.h
@@ -9,15 +9,12 @@
  */
 class Solution {
 public:
-
     int depth(TreeNode* root){
-        if(root == NULL){
+        if(root == NULL)
             return 0;
-        }
         
-        if(root->left == NULL && root->right == NULL){
+        if(root->left == NULL && root->right == NULL)
             return 1;
-        }
         
         int dl = depth(root->left);
         int dr = depth(root->right);
@@ -27,17 +24,23 @@ public:
     
     bool isBalanced(TreeNode* root) {
         
-        if(!root){
+        if(root == NULL)
             return true;
-        }
         
         int l = depth(root->left);
         int r = depth(root->right);
         
-        if(abs(l-r) > 1){
+        if(abs(l-r) > 1)
             return false;
-        }
         
         return isBalanced(root->left) && isBalanced(root->right);
     }
 };
+
+/**
+ * 这道题很有意思！
+ * 主体部分的递归过程，“return isBalanced(root->left) && isBalanced(root->right);”，
+ * 说明在找到一个满足条件的节点（即，非平衡）时，向上返回结果（false）。
+ * 注意，这里需要将结果向上传递至最初的函数调用，需要获取是否存在非平衡节点，因此，只关注false即可
+ * 
+ * /
