a0ecd6d6e5c340044e7b59b1f69ae62038b3bccd
kli-cauc <603469018@qq.com>
2016-08-26 15:05:49 +0800

    Create 83RemoveDuplicatesfromSortedList.h

---
+++ List/83RemoveDuplicatesfromSortedList.h
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode *wp = head;
+        
+        while(wp != NULL){
+            if(wp->next != NULL){
+                if(wp->val == wp->next->val){
+                    wp->next = wp->next->next;
+                }else{
+                    wp = wp->next;
+                }
+            }else{
+                wp = wp->next;
+            }
+        }
+        return head;
+    }
+};
