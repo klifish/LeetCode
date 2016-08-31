663ba4abf1176dd016a97a9eae9ab2dc382ef6bb
kli-cauc <603469018@qq.com>
2016-08-31 20:23:58 +0800

    Update 24. Swap Nodes in Pairs.h

--- Uncategorized/24. Swap Nodes in Pairs.h
+++ Uncategorized/24. Swap Nodes in Pairs.h
@@ -1,1 +1,23 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head == NULL || head->next == NULL)return head;
+        
+        ListNode* work_pointer = head;
+        
+        while(work_pointer != NULL && work_pointer->next != NULL){
+            swap(work_pointer->val,work_pointer->next->val);
+            work_pointer = work_pointer->next->next;
+        }
+        
+        return head;
+    }
+};
