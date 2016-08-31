f128e3d0a66e001786463ef8417ce0f6cf7cd159
kli-cauc <603469018@qq.com>
2016-08-31 21:11:58 +0800

    Update 100. Same Tree.h

--- Tree/100. Same Tree.h
+++ Tree/100. Same Tree.h
@@ -1,1 +1,32 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        
+        if(p == NULL && q == NULL){
+            return true;
+        }
+        
+        if(p == NULL && q != NULL){
+            return false;
+        }
+        
+        if(p != NULL && q == NULL){
+            return false;
+        }
+        
+        bool l = isSameTree(p->left,q->left);
+        bool r = isSameTree(p->right,q->right);
+        
+        return l && r && (p->val == q->val);
+        
+    }
+};
