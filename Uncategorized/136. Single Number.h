989a7200b508b909f25e38ae008b525baeb81e17
kli-cauc <603469018@qq.com>
2016-08-31 21:56:41 +0800

    Update 136. Single Number.h

--- Uncategorized/136. Single Number.h
+++ Uncategorized/136. Single Number.h
@@ -17,3 +17,18 @@ public:
  * 以上代码性能太差，优化
  * */
  
+/**
+ * 我自己确实没有往这方面想，挺牛逼的想法
+ * */
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = nums[0];
+        
+        for(int i = 1; i < nums.size();++i){
+            result = result ^ nums[i];
+        }
+        
+        return result;
+    }
+};
