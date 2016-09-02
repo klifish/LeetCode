07620773da87cee8dae08ba111e567865cf9f652
kli-cauc <603469018@qq.com>
2016-09-02 14:15:56 +0800

    Create 383. Ransom Note.h

---
+++ Uncategorized/383. Ransom Note.h
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> m;
+        for(auto c: magazine){
+            m[c]++;
+        }
+        
+        for(auto c:ransomNote){
+            m[c]--;
+            if(m[c] < 0){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
