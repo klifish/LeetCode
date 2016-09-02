0323e206d4bba74fa8d4bc4e148c56cbae8375a0
kli-cauc <603469018@qq.com>
2016-09-02 14:54:28 +0800

    Create 387. First Unique Character in a String.h

---
+++ Uncategorized/387. First Unique Character in a String.h
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        
+        int n = s.length();
+        
+        if(n == 0)
+            return -1;
+        
+        unordered_map<char,int> m;
+        unordered_map<char,int> m1;
+        
+        for(int i = 0; i < n; ++i){
+            m[s[i]] = i;
+            m1[s[i]]++;
+        }
+        
+        for(int i = 0; i < n; ++i){
+            if(m[s[i]] == i && m1[s[i]] == 1){
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
