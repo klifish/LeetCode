38c18fd74af1bce74fc39104d869fa247f92f003
kli-cauc <603469018@qq.com>
2016-09-02 15:28:46 +0800

    Update 26. Remove Duplicates from Sorted Array.h

--- Uncategorized/26. Remove Duplicates from Sorted Array.h
+++ Uncategorized/26. Remove Duplicates from Sorted Array.h
@@ -16,4 +16,35 @@ public:
         return nums.size();
     }
 };
-
+/**
+ * 慢
+ * */
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        if(n == 0){
+            return 0;
+        }
+        
+        int i = 0;
+        int j = 0;
+        
+        while(j < n){
+            if(nums[i] == nums[j]){
+                j++;
+            }else{
+                i++;
+                nums[i] = nums[j];
+                j++;
+            }
+        }
+        
+        return i+1;
+    }
+};
+/**
+ * 更慢！！！
+ * */
