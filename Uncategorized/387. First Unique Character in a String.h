8e41065d679ec7a96f26a3b0d0405c32e3dfb983
kli-cauc <603469018@qq.com>
2016-09-02 15:14:38 +0800

    Update 387. First Unique Character in a String.h

--- Uncategorized/387. First Unique Character in a String.h
+++ Uncategorized/387. First Unique Character in a String.h
@@ -24,3 +24,34 @@ public:
         return -1;
     }
 };
+/**
+ * 说实在的，这性能，我都看不下去了
+ * */
+ 
+ /**
+  * 优化——好一些
+  */
+  class Solution {
+public:
+    int firstUniqChar(string s) {
+        
+        int n = s.length();
+        
+        if(n == 0)
+            return -1;
+        
+        unordered_map<char,int> m1;
+        
+        for(int i = 0; i < n; ++i){
+            m1[s[i]]++;
+        }
+        
+        for(int i = 0; i < n; ++i){
+            if(m1[s[i]] == 1){
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
